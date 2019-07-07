33033b3fee6528668fbd309ea476cbc18db1a0b8
maksimanom <maksymKozoriz@gmail.com>
2019-07-07 17:58:39 +0300

    added handleClick

--- src/less5.js
+++ src/less5.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Lesson5 extends React.Component {
   constructor() {
@@ -8,28 +7,32 @@ class Lesson5 extends React.Component {
       names: ['Коля', 'Вася', 'Петя', 'Иван', 'Дима']
     }
   }
-
-  addItem() {
-    let text = "item added by pressing a button";
-    this.state.names.push(text);
-    this.setState({ names: this.state.names });
-  }
   deleteItem(num){
     this.state.names.splice(num, 1);
     this.setState({names : this.state.names});
   }
+  handleClick(event){
+    console.log(event);
+    const name = event.target.name;
+    if(name === "addItem"){
+      let text = "item added by pressing a button";
+      this.state.names.push(text);
+      this.setState({ names: this.state.names });
+    }
+  };
 
   render() {
     let list = this.state.names.map((item, index) => {
       return <li key={index}>{item} <button onClick={this.deleteItem.bind(this, index)}>delete me</button></li>
     });
+    
     return (
       <div>
         <ul>
           {list}
         </ul>
-        <button onClick={this.addItem.bind(this)}>Add item</button>
-        <button onClick={this.deleteItem.bind(this, this.state.names.length-1)}>Delete last item</button>
+        <button name="addItem" onClick={this.handleClick.bind(this)}>Add item with handleClick</button>
+        <button name="deleteLastItem" onClick={this.deleteItem.bind(this, this.state.names.length-1)}>Delete last item</button>
       </div>
     );
   }
