5539d642934a68fae7cd03ef5de8d8adbeab4801
maksimanom <maksymKozoriz@gmail.com>
2019-07-08 01:40:02 +0300

    task 4 done

--- src/Less6.js
+++ src/Less6.js
@@ -1,63 +1,108 @@
-// import React from 'react';
-
-// class Lesson6 extends React.Component {
-// 	constructor(){
-// 		super();
-// 		this.state= {
-// 			a: 1
-// 		}
-// 	}
-// 	handleClick(){
-// 		this.state.a++;
-// 		this.setState({a: this.state.a})
-// 	}
-// 	render() {
-// 	return (
-// 		<div onClick={this.handleClick.bind(this)}>
-// 			{this.state.a}
-
-// 		</div>
-// 	)
-// }
-// }
-
-// export default Lesson6;
 import React from 'react';
+// THAT WAS TRAINING UNDER (COMMITED), THIS IS TASKS:
+const Lesson62 = () => {
+	const [value, setValue] = React.useState("");
+	const handleChange = (event) => {
+		setValue(event.target.value.toUpperCase());
+	}
+	return(
+		<>
+			<form>
+				<input onChange={handleChange} name="text"/>
+			</form>
+			<div>
+				{value}
+			</div>
+		</>
+	)
+}
+const Lesson63 = () => {
+	const [value, setValue] = React.useState("");
+	const todayYear = new Date().getFullYear();
+	const handleChange = (event) => {
+		setValue(event.target.value);
+	}
+	return(
+		<>
+			<form>
+				<input onChange={handleChange} name="text" type="number"/>
+			</form>
+			<div>
+				{todayYear - value}
+			</div>
+		</>
+	)
+}
+const Lesson64 = () => {
+	const [values, setValues] = React.useState([]);
+	const handleChange = ((event) => {
+		setValues(event.target.value.split(/\s+/));
+		console.log(values);
+	});
+	const names = values.splice(0, 3);
+	while (names.length < 3) {
+    names.push('');
+  }
+	return(
+		<>
+			<input name="text" onChange={handleChange}/>
+			{names.map((name, index) => <p key={index}>{name}</p>)}
+		</>
+	)
+}
 
 
 const Lesson6 = () => {
-	const [items, setItems] = React.useState([1,2,3,4,5]);
-	const [value, setValue] = React.useState("");
-	const handleClick = (event) => {
-		console.log(items);
-		return (setItems(items.concat({value})));
-	};
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		return (setItems(items.concat(value)));
-	};
-	const handleChange = (event) => {
-		setValue(event.target.value);
-		console.log("value = " + value);
-	};
-	const list = items.map((item,index) => {
-		return( <li key={index}> {item} </li> )
-	});
 	return(
 		<>
-		<ul>
-			{items.map((item,index) => {
-				return( <li key={index}> {item} </li> )
-			})}
-		</ul>
-		<form onSubmit={handleSubmit}>
-			<input name="textToAdd" onChange={handleChange} />
-			<input type="submit"/>
-		</form>
+		{/* LEsson 6, task 1 + task 2 : */}
+		<Lesson62/>
+		<hr></hr>
+		{/* LEsson 6, task 3 : */}
+		<Lesson63/> 
+		<hr></hr>
+		{/* LEsson 6, task 4 : */}
+		<Lesson64/> 
 		</>
-	);
-	
+	)
 }
 
 export default Lesson6;
+
+
+// const Lesson6 = () => {
+// 	const [items, setItems] = React.useState([1,2,3,4,5]);
+// 	const [value, setValue] = React.useState("");
+// 	const handleClick = (event) => {
+// 		console.log(items);
+// 		return (setItems(items.concat({value})));
+// 	};
+// 	const handleSubmit = (event) => {
+// 		event.preventDefault();
+// 		return (setItems(items.concat(value)));
+// 	};
+// 	const handleChange = (event) => {
+// 		setValue(event.target.value);
+// 		console.log("value = " + value);
+// 	};
+// 	const list = items.map((item,index) => {
+// 		return( <li key={index}> {item} </li> )
+// 	});
+// 	return(
+// 		<>
+// 		<ul>
+// 			{items.map((item,index) => {
+// 				return( <li key={index}> {item} </li> )
+// 			})}
+// 		</ul>
+// 		<form onSubmit={handleSubmit}>
+// 			<input name="textToAdd" onChange={handleChange} />
+// 			<input type="submit"/>
+// 		</form>
+// 		</>
+// 	);
+	
+// }
+
+
 
