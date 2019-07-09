f23c7d1686baa3627426c578162c7c7f499cc834
maksimanom <maksymKozoriz@gmail.com>
2019-07-09 20:03:45 +0300

    task1 done

--- src/Less7.js
+++ src/Less7.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Lesson7Theory = (()=> {
-    const [option, setOption] = React.useState();
-    const handleRadioChange = ((event)=>{
-        setOption(event.target.value);
-    });
+const Task1 = (()=>{
+    const [value, setValue] = React.useState();
+    const handleTextareaChange = ((event)=>{
+        setValue(event.target.value);
+    })
     return(
         <>
-        <p>Your choise: {option}</p>
-        <input type="radio" value="option1" checked={option=="option1"} onChange={handleRadioChange} />
-        <input type="radio" value="option2" checked={option=="option2"} onChange={handleRadioChange} />
+        <textarea onChange={handleTextareaChange}></textarea>
+        <p>{value}</p>
         </>
     )
-});
+})
 
 
 const Lesson7 = (()=>{
     return (
-        <Lesson7Theory/>
+        <Task1/>
+
     )
 });
 
