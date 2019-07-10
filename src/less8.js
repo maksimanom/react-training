5f34ff396228bbe9185e51fa5b1935e04d500d2e
maksimanom <maksymKozoriz@gmail.com>
2019-07-10 20:25:30 +0300

    task 1 done

--- src/less8.js
+++ src/less8.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+const Task1 = (()=>{
+  const textDeco = {
+    textDecoration: "line-through"
+  }
+  const [elements, setElements] = React.useState([{ elem: "q", checked: false }, { elem: "w", checked: false }, { elem: "e", checked: false }, { elem: "r", checked: false }, { elem: "t", checked: false }, { elem: "y", checked: false}]);
+  const onCheckboxChange = (item)=>{
+    item.checked=!item.checked;
+    setElements(elements.concat([]))
+  }
+  return(
+    <ul>
+      {elements.map((item, index)=>{
+        return <li key={index} style={item.checked ? textDeco : {}}> <input type="checkbox" value={index} checked={item.checked} onChange={()=>onCheckboxChange(item)}/> {item.elem}</li>
+      })}
+    </ul>
+  )
+})
 
 const Lesson8 = (()=>{
   return(
     <>
-      {/* initial commit */}
+      <Task1 />
+      <hr></hr>
     </>
   )
 })
