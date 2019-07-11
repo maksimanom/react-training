3b95b74acb97b4dd0af165d19d69a6041d958d34
maksimanom <maksymKozoriz@gmail.com>
2019-07-11 15:33:38 +0300

    task 2 Calc not working

--- src/less8.js
+++ src/less8.js
@@ -16,11 +16,79 @@ const Task1 = (()=>{
     </ul>
   )
 })
+const Task2 = (() => {
+  const [employee, setEmployee] = React.useState([
+    {name: "Max", surname: "Kozoriz", rate: 1000, checked: false },
+    {name: "KekName", surname: "KekSurname", rate: 2000, checked: false },
+    {name: "LolName", surname: "LolSurname", rate: 3000, checked: false }
+  ]);
+  const [sum, setSum] = React.useState(0);
+
+  const sumCalc = ()=>{
+    const rateArray = employee.map((item, index)=>{
+      if (item.checked===true){
+        console.log("element taked: " + item.rate);
+        return item.rate;
+      }
+    });
+    console.log(rateArray);
+    const sumOfCheckedElements = rateArray.reduce((a, b) => a + b, 0);
+    setSum(sumOfCheckedElements)
+  }
+  const handleCheckboxChange = (item)=>{
+    item.checked = !item.checked;
+    setEmployee(employee.concat([]));
+    sumCalc();
+  }
+
+
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Employers info</th>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>Surname</td>
+          <td>Rate</td>
+        </tr>
+      </thead>
+      <tbody>
+        {employee.map((item, index) => (
+          <tr key={index} >
+          <td>
+            {item.name}
+          </td>
+          <td>
+            {item.surname}
+          </td>
+          <td>
+            {item.rate}
+          </td>
+            <td>
+              <input type="checkbox" value={item.id} onChange={()=>handleCheckboxChange(item)}/> {item.checked ? "checked" : "no"}
+            </td>
+        </tr>
+         ))}
+      </tbody>
+      <tfoot>
+        <tr style={{textAlign: "right"}}>
+          <td colSpan={3}>
+            Sum: {sum}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  )
+})
 
 const Lesson8 = (()=>{
   return(
     <>
       <Task1 />
+      <hr></hr>
+      <Task2 />
       <hr></hr>
     </>
   )
