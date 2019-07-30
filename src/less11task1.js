970382d4141253b3a51e21cab75c4c2418ed1762
maksimanom <maksymKozoriz@gmail.com>
2019-07-30 12:50:08 +0300

    task 1 done

--- src/less11task1.js
+++ src/less11task1.js
@@ -13,14 +13,29 @@ const employeeFromServer = [
   {name: "Oleg", surname: "Novitsky", daysWorked: 0, rate: 0},
 ];
 
-const EmployeeView = ({employee, num})=>{
+const EmployeeView = ({ employee, num, changeEmployeeData})=>{
+  const refDaysWorked = React.useRef(0);
+  const refRate = React.useRef(0);
+  const handleInputChange = ()=>{
+    if (refDaysWorked.current.value === 0 || refDaysWorked.current.value === NaN || refDaysWorked.current.value === "") {
+      changeEmployeeData(num, 0, parseFloat(refRate.current.value));
+      return 0
+    }
+    if (refRate.current.value === 0 || refRate.current.value === NaN || refRate.current.value === "") {
+      changeEmployeeData(num, parseFloat(refDaysWorked.current.value), 0);
+      return 0
+    }
+    changeEmployeeData(num, parseFloat(refDaysWorked.current.value), parseFloat(refRate.current.value));
+  }
 
   return(
     <tr>
       <td>{employee.name}</td>
       <td>{employee.surname}</td>
-      <td>{employee.daysWorked}</td>
-      <td>{employee.rate}</td>
+      <td> 
+        <input type="number" name="daysWorked" id={"daysWorked"+employee.name} ref={refDaysWorked} onChange={handleInputChange}/>
+      </td>
+      <td> <input type="number" name="rate" id={"rate" + employee.name} ref={refRate} onChange={handleInputChange}/> </td>
       <td>{employee.daysWorked*employee.rate}</td>
     </tr>
   )
@@ -28,6 +43,20 @@ const EmployeeView = ({employee, num})=>{
 
 const App = ()=>{
   const [employeeArray, setEmployeeArray] = React.useState(employeeFromServer);
+
+  const changeEmployeeData = (num, newDaysWorked, newRate)=>{
+    employeeArray[num] = { name: employeeArray[num].name, surname: employeeArray[num].surname, daysWorked: newDaysWorked, rate: newRate};
+    setEmployeeArray(employeeArray.concat([]));
+  }
+  const SumTotal = ()=>{
+    // let sum = (acc, curV) => parseFloat(acc.rate) + parseFloat(curV.rate);
+    // let sumTotal = employeeArray.reduce(sum);
+    let sumTotal=0;
+    employeeArray.map((item, index)=>{
+      sumTotal+=item.rate*item.daysWorked;
+    })
+    return sumTotal    
+  }
 
   return(
     <table>
@@ -42,7 +71,12 @@ const App = ()=>{
       </thead>
       <tbody>
         {employeeArray.map((item, index)=>{
-          return <EmployeeView key={index} employee={item} num={index}/>
+          return( <EmployeeView
+            key={index}
+            employee={item}
+            num={index}
+            changeEmployeeData={(num, newDaysWorked, newRate) => changeEmployeeData(num, newDaysWorked, newRate)}/>
+          )
         })}
       </tbody>
       <tfoot>
@@ -51,7 +85,7 @@ const App = ()=>{
           <td>Surname</td>
           <td>Days Worked</td>
           <td>Rate</td>
-          <td>Total</td>
+          <td style={{color: "red"}}> <SumTotal /> </td>
         </tr>
       </tfoot>
     </table>
