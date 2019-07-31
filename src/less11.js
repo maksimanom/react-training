0509f85e8711403beb1f267887f7c6285b942518
maksimanom <maksymKozoriz@gmail.com>
2019-07-31 20:09:06 +0300

    task 3 started (none)

--- src/less11.js
+++ src/less11.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App1 from './less11task1';
 import App2 from './less11task2';
+import App3 from './less11task3';
 
 const Lesson11=()=>{
   
@@ -9,6 +10,8 @@ const Lesson11=()=>{
     <App1 />
     <br /> <hr /> <br/>
     <App2 />
+    <br /> <hr /> <br />
+    <App3 />
     </>
   )
 }
---
+++ src/less11task3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App3 = ()=>{
+
+  return(
+    <>
+      <h1>SANYA PIDOR</h1>
+    </>
+  )
+}
+export default App3
