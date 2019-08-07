5df8cb0e36c849bf3afb93e5106475ee13b26983
maksimanom <maksymKozoriz@gmail.com>
2019-08-07 18:33:42 +0300

    task 8 (tic tac toe)

--- src/less11.js
+++ src/less11.js
@@ -2,7 +2,9 @@ import React from "react";
 // import App1 from './less11task1';
 // import App2 from './less11task2';
 // import App3 from './less11task3';
-import App4 from './less11task4';
+// import App4 from './less11task4';
+import App7 from './less11task7';
+import App8 from './less11task8';
 
 const Lesson11=()=>{
   
@@ -13,8 +15,12 @@ const Lesson11=()=>{
     <App2 />
     <br /> <hr /> <br />
     <App3 /> */}
+    {/* <br /> <hr /> <br />
+    <App4 /> */}
+    {/* <br /> <hr /> <br />
+    <App7 /> */}
     <br /> <hr /> <br />
-    <App4 />
+    <App8 />
     </>
   )
 }
