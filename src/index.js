c9dbbf880becef04b8c842abd944fadb867135f4
maksimanom <maksymKozoriz@gmail.com>
2020-02-14 12:09:16 +0200

    deleted trash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lesson11 from './less11';
 
 ReactDOM.render(
-  <Lesson11 />,
+  <>
+  
+  </>,
   document.getElementById('root')
 );
